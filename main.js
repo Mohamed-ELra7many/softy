d7d5ee9ad2aad1d757bbb790db4b15be74ef050b
Mohamed-ELra7manysettings <mohamed231adbo@gmail.com>
2022-03-31 21:56:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+{   //To add and delete classes to a menu item 
+
+let IconMenu = document.querySelector(".menu")
+let NavMenu = document.querySelector(".nav-menu")
+
+IconMenu.addEventListener("click", ()=>{
+    NavMenu.classList.toggle("visabl")
+})
+}
+
+{   // This is when the page scroll happens, the following happens
+
+let About = document.getElementById("about")
+let Allfeatures = document.querySelectorAll(".features .image")
+let mission = document.querySelector(".mission")
+let Allnumber = document.querySelectorAll(".number")
+let start = false;
+
+
+let counter = (el)=>{   //Then the numbers are incremented inside the mission element
+    let count = el.dataset.number;
+    let interval = setInterval(() => {
+        el.textContent++
+        if(el.textContent == count){
+            clearInterval(interval)
+        }
+    }, 2000 / count);
+}
+
+
+window.onscroll = function(){
+    if (window.scrollY >= About.offsetTop -200) {   // first, the two images are moved left and right, which is in the abuot element
+        Allfeatures.forEach((item)=>{
+            item.style.transform= "translateX(0vw)"
+        })
+    }else{
+        Allfeatures.forEach((item)=>{
+        item.style.transform = "translateX(-10vw)"
+        })
+    }
+
+if (window.scrollY >= mission.offsetTop - 400) {   //Then the numbers are incremented inside the mission element
+        if(!start){
+            Allnumber.forEach((item) => counter(item))
+        }
+        start = true
+    }
+}
+}
+
